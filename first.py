eb45243e9290878bc9a479e87e233b87e23683c3
kammitama5 <krystal.maughan@gmail.com>
2016-06-11 20:24:10 -0700

    intro to python
    intro to Python at hacker meet

---
+++ first.py
@@ -0,0 +1,43 @@
+##Welcome message --> game begin
+
+print 'Welcome to my game. This game is sweet.'
+##ask the player for his name
+player_name = raw_input('What\'s is your name: ')
+
+## check/ask for player name and display it
+if player_name == '':
+	print'\n'
+	print 'You entered an empty string!'
+else:
+	
+	print 'Your name is {}'.format(player_name)
+
+## check/ask for player age and display it
+page =  raw_input('What is your age: ')
+if page.isdigit():
+	page = int(page)
+	print 'Your age is {}'.format(page)
+	print'\n'
+else:
+	print 'Not a real age'
+	print'\n'
+
+## list gamestuff var and ask them to choose
+gamestuff = ['dragons','dungeons','mazes','the single life']	
+
+
+while True:
+	print 'What shall we do?'
+	for item in gamestuff:
+		print '* {}'.format(item)
+
+## choose gamestuff or else not an answer (or exit to quit)	
+	choice = raw_input('>>>')
+	if choice.lower() in ['exit','quit']:
+		print 'You have chosen to quit.'
+		break;
+	if choice not in gamestuff:
+		print 'come on now. That is not a choice.'
+	else:
+		print 'you chose "{}"'.format(choice)
+	
